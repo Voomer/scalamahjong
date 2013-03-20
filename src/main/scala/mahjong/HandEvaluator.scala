d602ef05836f23ea187e4afa1b48c3030d42f1e5
The Grimace <Nightvoomer@gmail.com>
2013-03-20 00:01:47 -0500

    more progress on hand evalutator

--- src/main/scala/mahjong/HandEvaluator.scala
+++ src/main/scala/mahjong/HandEvaluator.scala
@@ -2,13 +2,11 @@ package mahjong
 
 object HandEvaluator {
   
-	lazy val map = Map(1->"one",
-	    3->"two",
-	    4->"two")
-  
+	
 	def ShantenMethod1(tiles:List[Tile]):Int= {
-	  val remainingTiles = removePairSets(removeThreeSets(tiles))
-	  remainingTiles.length
+	  var bin = Array[Int](36)
+	  tiles foreach (t => bin(TileConvert(t)) += 1)
+	  return 1+ 1
 	}
 
 	
@@ -18,8 +16,7 @@ object HandEvaluator {
 	   case List(a:SuitTile, b:SuitTile, c:SuitTile) if a.name == b.name == c.name =>
 	     a.rank+2 == b.rank+1 == c.rank
 	   case _ => false
-	}
-	
+	}	
 	
 	def removeThreeSets(tiles:List[Tile]):List[Tile] = {
 	  var count = 0
@@ -50,5 +47,23 @@ object HandEvaluator {
 	    }
 	  }
 	  mutList
+	}
+	
+	object TileConvert{
+	  lazy val tileToInt = {
+	    val tiles = Hand("1b 2b 3b 4b 5b 6b 7b 8b 9b 1c 2c 3c 4c 5c 6c 7c 8c 9c 1d 2d 3d 4d 5d 6d 7d 8d 9d w e s n r g b");
+	    val nums = 0 to 35
+	    tiles.zip(nums).toMap
+	  }
+	  
+	  lazy val intToTile = tileToInt map {_.swap}
+	  
+	  def apply(tile:Tile):Int = {
+		  tileToInt(tile)
+	  }
+	  
+	  def apply(int:Int):Tile = {
+		  intToTile(int)
+	  } 
 	}
 }
